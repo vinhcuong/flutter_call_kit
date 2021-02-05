6032070644a2e34abc238200e4c34b9b5b352001
Đồng Anh Vĩnh Cường <cuongdav@vnpt.vn>
2021-02-05 15:18:46 +0700

    delete code

--- ios/Classes/FlutterCallKitPlugin.m
+++ ios/Classes/FlutterCallKitPlugin.m
@@ -1,22 +1,3 @@
-#import <Flutter/Flutter.h>
-#import <UIKit/UIKit.h>
-#import <CallKit/CallKit.h>
-#import <Intents/Intents.h>
-
-API_AVAILABLE(ios(10.0))
-@interface FlutterCallKitPlugin : NSObject<FlutterPlugin, CXProviderDelegate>
-@property (nonatomic, strong) CXCallController * _Nullable callKitCallController;
-@property (nonatomic, strong) CXProvider * _Nullable callKitProvider;
-
-+ (void)reportNewIncomingCall:(NSString *_Nonnull)uuidString
-                       handle:(NSString *_Nonnull)handle
-                   handleType:(NSString *_Nonnull)handleType
-                     hasVideo:(BOOL)hasVideo
-          localizedCallerName:(NSDictionary * _Nullable)localizedCallerName
-                  fromPushKit:(BOOL)fromPushKit;
-@end
-
-
 #import "FlutterCallKitPlugin.h"
 
 #import <AVFoundation/AVAudioSession.h>
